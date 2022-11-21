569860a1026cd90805a53c4cebc6a1de0f150365
ST <workinvs@gmail.com>
2022-11-21 14:06:51 +0200

    check now

--- src/templates/AuthTemplate/index.tsx
+++ src/templates/AuthTemplate/index.tsx
@@ -37,7 +37,7 @@ export default function AuthTemplate() {
             }, 2000);
         }
 
-    }, [])
+    }, [oobCode])
     const {
         loginModal: { showModal, toggleModal },
     }: any = useModal();
